33e2999585e99253eaae3212d0b3818076e12bb4
nettlespike <46365058+nettlespike@users.noreply.github.com>
2021-11-21 11:46:50 -0500

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -4,8 +4,8 @@ export default function Home() {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen py-2">
       <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
+        <title>Bubbletea Tracker</title>
+        <link rel="icon" href="/bbticon.svg" />
       </Head>
 
       <main className="flex flex-col items-center justify-center w-full flex-1 px-20 text-center">
