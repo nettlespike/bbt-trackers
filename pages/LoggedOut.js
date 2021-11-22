4d8586ba7419a14b8c84f71088e9a08cf411d1bd
nettlespike <46365058+nettlespike@users.noreply.github.com>
2021-11-21 23:43:58 -0500

    Update LoggedOut.js

--- pages/LoggedOut.js
+++ pages/LoggedOut.js
@@ -1,3 +1,4 @@
+import Link from "next/link";
 export default function LoggedOut() {
     <button onclick="window.location.href='http://localhost:3000/';">
     Click Here
@@ -5,14 +6,22 @@ export default function LoggedOut() {
 
 
     return (
-        <div className="relative bg-white flex justify-center w-screen">
+        <div className="relative bg-white flex flex-col overflow-auto justify-center w-screen">
             <div className="flex flex-col justify-start space-y-10 pt-32 z-10">
                 <div className="flex flex-col text-center">
                     <div className="text-center text-5xl text-indigo-800">
                         You are now logged out
                     </div>
                 </div>
+                <Link href = "/"> 
+                    <input
+                        className = "flex object-none justify-center bg-indigo-500 hover:bg-indigo-700 rounded x2 text-white p-2"
+                        type="submit"
+                        value="Login Again"
+                    />
+                 </Link>
             </div>
+            
         </div>
     )
 
