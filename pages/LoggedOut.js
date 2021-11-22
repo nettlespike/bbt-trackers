845a256167ef6c21f03a05d24ae5ea594710a5d4
OwenTheEpicGamer <88258850+OwenTheEpicGamer@users.noreply.github.com>
2021-11-21 23:51:38 -0500

    loggedoutresponsive

--- pages/LoggedOut.js
+++ pages/LoggedOut.js
@@ -2,14 +2,14 @@ import React from "react";
 import Link from "next/link";
 export default function LoggedOut() {
     return (
-      <><div className="relative bg-white flex justify-center w-screen">
+      <><div className="w-screen relative bg-white flex justify-center">
             <div className="flex flex-col justify-start space-y-10 pt-32 z-10">
                 <div className="flex flex-col justify-center items-center text-center space-y-8">
-                    <div className="text-center text-5xl text-indigo-800">
+                    <div className="text-center text-3xl md:text-4xl lg:text-5xl text-indigo-800">
                         You are now logged out
                     </div>
                     <Link href="/">
-                        <button className="text-xl w-1/3 rounded-xl bg-purple-300 p-4">Log back in</button>
+                        <button className="text-xl w-1/2 md:w-3/4 md:text-2xl lg:w-4/4 lg:text-3xl rounded-xl bg-purple-300 p-1 md:p-2 lg:p-4">Log back in</button>
                     </Link>
                     <div className="flex justify-center items-center text-2xl">
                         <img 
