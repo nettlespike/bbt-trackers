37643d5bdca6dce02e0a8c1e3e7a3d232c649d9e
OwenTheEpicGamer <88258850+OwenTheEpicGamer@users.noreply.github.com>
2021-11-21 23:20:23 -0500

    loggedout

---
+++ pages/LoggedOut.js
@@ -0,0 +1,21 @@
+export default function LoggedOut() {
+    <button onclick="window.location.href='http://localhost:3000/';">
+    Click Here
+    </button>
+
+
+    return (
+      <><div className="relative bg-white flex justify-center w-screen">
+            <div className="flex flex-col justify-start space-y-10 pt-32 z-10">
+                <div className="flex flex-col text-center">
+                    <div className="text-center text-5xl text-indigo-800">
+                        You are now logged out
+                    </div>
+                </div>
+            </div>
+        </div><button onclick="location.href='http://www.google.com'" type="button">
+                www.example.com</button></>
+    )
+
+
+}
